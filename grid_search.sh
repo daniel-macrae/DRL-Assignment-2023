fb850e649b8527748ab9ec4787b88f4fb0679f5d
daniel-macrae <d.macrae@student.rug.nl>
2023-05-09 20:27:28 +0200

    req

--- grid_search.sh
+++ grid_search.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=testingSSDLite
-#SBATCH --time=24:00:00
+#SBATCH --time=00:30:00
 #SBATCH --cpus-per-task=1
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
@@ -8,6 +8,6 @@
 #SBATCH --mail-type=FAIL,END
 #SBATCH --mail-user=d.macrae@student.rug.nl
 module load Miniconda3
-conda create -n deppRL python=3.8
-source activate deppRL
+conda create -n deepRL python=3.8
+source activate deepRL
 pip install -r requirements.txt
