c51c04b0d22851f2eccd2a21d317f029fc921ada
daniel-macrae <d.macrae@student.rug.nl>
2023-06-07 17:25:31 +0200

    Update trainOptimised.sh

--- Part2/trainOptimised.sh
+++ Part2/trainOptimised.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=PPO
-#SBATCH --time=4:00:00
+#SBATCH --time=5:00:00
 #SBATCH --cpus-per-task=1
 #SBATCH --partition=regular
-#SBATCH --mem=8GB
+#SBATCH --mem=4GB
 #SBATCH --mail-type=FAIL,END
 #SBATCH --mail-user=d.macrae@student.rug.nl
 module load Miniconda3
